c26b1a78894e250610450121a72e18cb002abd82
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-01-01 09:33:19 -0300

    Improve Auth Login Exception

--- app/Exceptions/InvalidAuthenticationException.php
+++ app/Exceptions/InvalidAuthenticationException.php
@@ -2,21 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Traits\RenderToJson;
 use Exception;
 
 class InvalidAuthenticationException extends Exception
 {
-    protected $message = 'Invalid credentials';
+    use RenderToJson;
 
+    protected $message = 'Invalid credentials';
     protected $code = 401;
 
-    public function render()
-    {
-        return response()->json(
-            [
-                'error' => class_basename($this),
-                'message' => $this->message
-            ],
-            $this->getCode());
-    }
 }
